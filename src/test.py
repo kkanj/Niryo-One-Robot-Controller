5f23d5c3e419b7292cb16045ff98bbb0de6083ea
Krishan Kanji <krishankanji2003@gmail.com>
2025-01-01 00:14:44 -0800

    test.py to test robot movement

---
+++ src/test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from niryo_one_python_api.niryo_one_api import *
+import rospy
+import time
+
+rospy.init_node('niryo_one_test_move_up')
+
+print("--- Start")
+
+n = NiryoOne()
+
+try:
+    # Calibrate robot first
+    n.calibrate_auto()
+    print("Calibration finished!\n")
+
+    time.sleep(1)
+
+    # Move up
+    n.move_pose(0, 0, 0.1, 0, 0, 0)  # Move up by 0.1 meters
+
+except NiryoOneException as e:
+    print(e)
+
+print("--- End")
